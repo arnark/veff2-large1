c5aab1050d0f9bbecc19f5d38b36e8bb36312d22
Arnarkj <arnark18@ru.is>
2020-01-27 05:01:41 +0000

    Added parent() function

--- makeBelieve.js
+++ makeBelieve.js
@@ -1,45 +1,82 @@
-// Definition of the chainable function class __
+/* Definition of the chainable function class __ */
 class __ {
+
+  /* The constructor takes in a element query and returns found elements if they exist */
   constructor(query) {
     // Wait for page to load
     window.addEventListener('load', function () {
       // Get all elements with the given query selectors
       const elements = document.querySelectorAll(query);
-      // Convert NodeList to Array
-      const elementsArr = Array.prototype.slice.call(elements);
-      this.elements = elementsArr;
-
-      // Demonstration with testHtml.html, log the first element if found, otherwise undefined
-      console.log(this.elements[0])
+      this.elements = elements;
     });
-    return this.elements;
   }
 
-  firstMethod() {
-    console.log(`hello ${this.query}`);
-    return this;
+  /* Helpers */
+  /* Get the element type from selector string (id, class or tagname) */
+  elementType(selector) {
+    if(typeof selector === 'string') {
+      const firstChar = selector.charAt(0);
+      if(firstChar === '#') {
+        return 'ID';
+      } else if(firstChar === '.') {
+        return 'CLASS';
+      } else {
+        return 'TAG';
+      }
+    }
+    return undefined;
   }
-   
-  secondMethod() {
-    console.log('This is the Second Method');
-      return this;
+
+  /* Get css selector stripped of first char if applicable */
+  getClassNameStripped(cssSelector) {
+    const elementType = this.elementType(cssSelector);
+    if(elementType === 'ID' || elementType === 'CLASS') {
+      return cssSelector.substr(1);
+    } else {
+      return cssSelector;
+    }
   }
-   
-  thirdMethod() {
-    console.log('This is the Third Method');
-      return this;
+
+  /* Main functions */
+  parent(cssSelector = undefined) {
+    // Get the stripped selector
+    cssSelector = this.getClassNameStripped(cssSelector);
+    let elementArr = [];
+    // Wait for document to load
+    window.addEventListener('load', function () {
+      // If selector is not provided
+      if(cssSelector == null) {
+        Array.from(this.elements).forEach(function (element) {
+            elementArr.push(element.parentNode);
+        });
+      } else {
+        // Iterate through the elements, checking if the given selector is the parent
+        Array.from(this.elements).forEach(function (element) {
+          // Hacky way to compare selector with id, class and tags. ###suggestions?
+          if(element.parentNode.className === cssSelector 
+            || element.parentNode.id === cssSelector
+            || element.parentNode.localName === cssSelector) {
+            elementArr.push(element.parentNode);
+          }
+        });
+      }
+    });
+    return elementArr;
   }
 
 }
 
-// Required to get rid of the new keyword for initiating classes
+// Required to get rid of the new keyword for initiating the __ class
 var _old = __;
 __ = function(...args) { return new _old(...args) };
 
-// Example of chainable methods
-__("test")
-  .firstMethod()
-  .secondMethod()
-  .thirdMethod();
 
-console.log(__("#container"))
+// Example of general query selector
+/* Þetta semsagt virkar ekki, vegna þess að constructorar 'returna' ekki. Reyni að finna lausn. */
+console.log(__("#container h2"));
+
+// Example of chainable methods and the parent function
+console.log(__("p").parent(".selected"));
+console.log(__("p").parent("#container"));
+console.log(__("p").parent("form"));
+console.log(__("p").parent());
